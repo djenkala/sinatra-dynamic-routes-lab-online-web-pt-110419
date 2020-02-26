fd034586893d6dc0836e62a2dc9971630abd3513
Daniel Jenkala <djenkala@yahoo.com>
2020-02-26 22:29:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,13 @@ class App < Sinatra::Base
  end
  
  get '/:operation/:number1/:number2' do
-   num1 = 
+   number1 = params[:number1].to_i
+   number2 = params[:number2].to_i
+   
+   answer = 'error'
+   
+   case params[:operation]
+   when
  end
 
 end
