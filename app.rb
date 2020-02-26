a0b02e57f679f2edd58fd70855ad287cce8bab9c
Daniel Jenkala <djenkala@yahoo.com>
2020-02-26 22:34:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,32 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
- 
- get '/reversename/:name' do
-   @reversename = params[:name].reverse
-   "#{@reversename}"
- end
- 
- get '/square/:number' do
-   @square = params[:number].to_i * params[:number].to_i
-   "#{@square}"
- end
- 
- get '/say/:number/:phrase' do
-   say = ''
-   
-   params[:number].to_i.times do
-     say += params[:phrase]
-   end
-   
-   say
- end
- 
- get '/say/:word1/:word2/:word3/:word4/:word5' do
-   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
- end
- 
-get '/:operation/:number1/:number2' do
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+
+    answer
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
@@ -43,9 +40,4 @@ get '/:operation/:number1/:number2' do
       answer = (number1 / number2).to_s
     end
   end
-end
-
-
-
-
 end
