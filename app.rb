dc06a89c5e75b85129c8d3e404b31986f858748f
Daniel Jenkala <djenkala@yahoo.com>
2020-02-26 22:26:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,6 +26,8 @@ class App < Sinatra::Base
    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
  end
  
- get 
+ get '/:operation/:number1/:number2' do
+   @answer = 
+ end
 
 end
